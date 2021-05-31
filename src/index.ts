7373e3047002ba530ddbbc0b7d14ec9fae95260d
YaGrig <yashagrigorev@gmail.com>
2021-05-31 23:17:09 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import './styles.scss';
 import { App } from './app';
 
 window.onload = () => {
+  alert('Извиняюсь за качество верстки, не хватило времени. С точки зрения функционала старался выполнить все. ')
   const appElement = document.getElementById('app');
   if (!appElement) throw Error('App root element not found');
 
